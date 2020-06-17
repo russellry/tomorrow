dc24f622d4403c85561e9d025df2cd9ba08543c4
russellry <digivicer@hotmail.com>
2020-06-18 01:37:18 +0800

    fix sign in with apple

--- tomorrow/Signup/SignupViewController.swift
+++ tomorrow/Signup/SignupViewController.swift
@@ -7,27 +7,15 @@
 //
 
 import UIKit
-import FBSDKLoginKit
 import AuthenticationServices
+import CryptoKit
+import Firebase
 
 class SignupViewController: UIViewController {
     
     var name = ""
-    let appleLoginButton: ASAuthorizationAppleIDButton = {
-        let button = ASAuthorizationAppleIDButton()
-        button.addTarget(self, action: #selector(handleLoginWithAppleID), for: .touchUpInside)
-        return button
-    }()
-    
-    let fbLoginButton: UIButton = {
-        let button = UIButton()
-        button.addTarget(self, action: #selector(handleLoginWithFB), for: .touchUpInside)
-        button.backgroundColor = .systemBlue
-        button.setTitleColor(.white, for: .normal)
-        button.setTitle("Sign in with Facebook", for: .normal)
-        button.layer.cornerRadius = 8
-        return button
-    }()
+    // Unhashed nonce.
+    fileprivate var currentNonce: String?
     
     @IBOutlet weak var nameLabel: UILabel!
     @IBOutlet weak var loginStackView: UIStackView!
@@ -36,88 +24,136 @@ class SignupViewController: UIViewController {
         super.viewDidLoad()
         setBackground(name: "onboard-bg")
         nameLabel.text = name
-        guard let _ = UserDefaults.standard.object(forKey: "user") as? String else {
-            if let token = AccessToken.current, !token.isExpired {
-                NSLog("Facebook token is: \(String(describing: AccessToken.current))")
-            } else {
-                NSLog("Facebook token has expired")
-                loginStackView.addArrangedSubview(fbLoginButton)
-                loginStackView.addArrangedSubview(appleLoginButton)
-            }
-            return
-        }
-        
+        setupProviderLoginView()
     }
     
-    func saveFBDetails(token: AccessToken) {
-        let tokenString = token.tokenString
-        let params = ["fields": "first_name, last_name, email"]
-        let graphRequest = GraphRequest(graphPath: "me", parameters: params, tokenString: tokenString, version: nil, httpMethod: .get)
-        graphRequest.start { (connection, result, error) in
-            
-            if let err = error {
-                print("Facebook graph request error: \(err)")
-            } else {
-                print("Facebook graph request successful!")
-                
-                guard let json = result as? NSDictionary else { return }
-                if let email = json["email"] as? String {
-                    print("\(email)")
-                }
-                if let firstName = json["first_name"] as? String {
-                    print("\(firstName)")
-                }
-                if let lastName = json["last_name"] as? String {
-                    print("\(lastName)")
-                }
-                if let id = json["id"] as? String {
-                    print("\(id)")
-                }
-            }
-        }
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        performExistingAccountSetupFlows()
     }
     
-    @objc func handleLoginWithFB() {
-        let manager = LoginManager()
-        manager.logIn(permissions: ["public_profile", "email"], viewController: self, completion: { [weak self] result in
-            guard let self = self else {
-                return
-            }
-            switch result {
-            case .success(granted: let granted, declined: let declined, token: let token):
-                self.saveFBDetails(token: token)
-            case .cancelled:
-                print("cancelled")
-            case .failed(_):
-                print("failed")
-            }
-        })
-    }
-    
-    @objc func handleLoginWithAppleID() {
-        let request = ASAuthorizationAppleIDProvider().createRequest()
-        request.requestedScopes = [.fullName, .email]
-        let controller = ASAuthorizationController(authorizationRequests: [request])
-        controller.delegate = self
-        controller.presentationContextProvider = self
-        controller.performRequests()
-    }
-    
+
 }
 
 extension SignupViewController: ASAuthorizationControllerDelegate {
+    
+    func setupProviderLoginView() {
+        let authorizationButton = ASAuthorizationAppleIDButton()
+        authorizationButton.addTarget(self, action: #selector(handleAuthorizationAppleIDButtonPress), for: .touchUpInside)
+        self.loginStackView.addArrangedSubview(authorizationButton)
+    }
+    
+    func performExistingAccountSetupFlows() {
+        // Prepare requests for both Apple ID and password providers.
+        let requests = [ASAuthorizationAppleIDProvider().createRequest(),
+                        ASAuthorizationPasswordProvider().createRequest()]
+        
+        // Create an authorization controller with the given requests.
+        let authorizationController = ASAuthorizationController(authorizationRequests: requests)
+        authorizationController.delegate = self
+        authorizationController.presentationContextProvider = self
+        authorizationController.performRequests()
+    }
+    
+    @objc func handleAuthorizationAppleIDButtonPress() {
+        startSignInWithAppleFlow()
+    }
+    
+    /// - Tag: did_complete_authorization
     func authorizationController(controller: ASAuthorizationController, didCompleteWithAuthorization authorization: ASAuthorization) {
-        switch authorization.credential {
-        case let appleIDCredential as ASAuthorizationAppleIDCredential:
-            let userIdentifier = appleIDCredential.user
+        if let appleIDCredential = authorization.credential as? ASAuthorizationAppleIDCredential {
+            guard let nonce = currentNonce else {
+                fatalError("Invalid state: A login callback was received, but no login request was sent.")
+            }
+            guard let appleIDToken = appleIDCredential.identityToken else {
+                print("Unable to fetch identity token")
+                return
+            }
+            guard let idTokenString = String(data: appleIDToken, encoding: .utf8) else {
+                print("Unable to serialize token string from data: \(appleIDToken.debugDescription)")
+                return
+            }
+            // Initialize a Firebase credential.
+            let credential = OAuthProvider.credential(withProviderID: "apple.com",
+                                                      idToken: idTokenString,
+                                                      rawNonce: nonce)
+            // Sign in with Firebase.
+            Auth.auth().signIn(with: credential) { (authResult, error) in
+                if (error != nil) {
+                    // Error. If error.code == .MissingOrInvalidNonce, make sure
+                    // you're sending the SHA256-hashed nonce as a hex string with
+                    // your request to Apple.
+                    print(error?.localizedDescription)
+                    return
+                }
+                // User is signed in to Firebase with Apple.
+                // ...
+            }
+        }
+    }
+    
+    // Adapted from https://auth0.com/docs/api-auth/tutorials/nonce#generate-a-cryptographically-random-nonce
+    private func randomNonceString(length: Int = 32) -> String {
+        precondition(length > 0)
+        let charset: Array<Character> =
+            Array("0123456789ABCDEFGHIJKLMNOPQRSTUVXYZabcdefghijklmnopqrstuvwxyz-._")
+        var result = ""
+        var remainingLength = length
+        
+        while remainingLength > 0 {
+            let randoms: [UInt8] = (0 ..< 16).map { _ in
+                var random: UInt8 = 0
+                let errorCode = SecRandomCopyBytes(kSecRandomDefault, 1, &random)
+                if errorCode != errSecSuccess {
+                    fatalError("Unable to generate nonce. SecRandomCopyBytes failed with OSStatus \(errorCode)")
+                }
+                return random
+            }
             
-            let defaults = UserDefaults.standard
-            defaults.set(userIdentifier, forKey: "user")
-            
-            performSegue(withIdentifier: "toHomeScreen", sender: nil)
-            break
-        default:
-            break
+            randoms.forEach { random in
+                if remainingLength == 0 {
+                    return
+                }
+                
+                if random < charset.count {
+                    result.append(charset[Int(random)])
+                    remainingLength -= 1
+                }
+            }
+        }
+        
+        return result
+    }
+    
+    func startSignInWithAppleFlow() {
+        let nonce = randomNonceString()
+        currentNonce = nonce
+        let appleIDProvider = ASAuthorizationAppleIDProvider()
+        let request = appleIDProvider.createRequest()
+        request.requestedScopes = [.fullName, .email]
+        request.nonce = sha256(nonce)
+        
+        let authorizationController = ASAuthorizationController(authorizationRequests: [request])
+        authorizationController.delegate = self
+        authorizationController.presentationContextProvider = self
+        authorizationController.performRequests()
+    }
+    
+    private func sha256(_ input: String) -> String {
+        let inputData = Data(input.utf8)
+        let hashedData = SHA256.hash(data: inputData)
+        let hashString = hashedData.compactMap {
+            return String(format: "%02x", $0)
+        }.joined()
+        
+        return hashString
+    }
+    
+    private func saveUserInKeychain(_ userIdentifier: String) {
+        do {
+            try KeychainItem(service: "com.example.apple-samplecode.juice", account: "userIdentifier").saveItem(userIdentifier)
+        } catch {
+            print("Unable to save userIdentifier to keychain.")
         }
     }
 }
@@ -125,5 +161,21 @@ extension SignupViewController: ASAuthorizationControllerDelegate {
 extension SignupViewController: ASAuthorizationControllerPresentationContextProviding {
     func presentationAnchor(for controller: ASAuthorizationController) -> ASPresentationAnchor {
         return self.view.window!
+    }
+    
+    func authorizationController(controller: ASAuthorizationController, didCompleteWithError error: Error) {
+        // Handle error.
+        print("Sign in with Apple errored: \(error)")
+    }
+}
+
+extension UIViewController {
+    func showSignupController() {
+        let storyboard = UIStoryboard(name: "SignupScreen", bundle: nil)
+        if let loginViewController = storyboard.instantiateViewController(withIdentifier: "SignupViewController") as? SignupViewController {
+            loginViewController.modalPresentationStyle = .formSheet
+            loginViewController.isModalInPresentation = true
+            self.present(loginViewController, animated: true, completion: nil)
+        }
     }
 }
