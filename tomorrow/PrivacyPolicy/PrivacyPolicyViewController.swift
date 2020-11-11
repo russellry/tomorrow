bc00b2b8521e678397bfe20c3019413544833c4c
russellry <digivicer@hotmail.com>
2020-11-12 00:05:09 +0800

    added terms of use, privacy policy

---
+++ tomorrow/PrivacyPolicy/PrivacyPolicyViewController.swift
@@ -0,0 +1,22 @@
+//
+//  PrivacyPolicyViewController.swift
+//  tomorrow
+//
+//  Created by Russell Ong on 11/11/20.
+//  Copyright © 2020 trillion.unicorn. All rights reserved.
+//
+
+import UIKit
+
+class PrivacyPolicyViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func didTapPrivacyPolicyBtn(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+}
---
+++ tomorrow/TermsOfUse/TermsOfUseViewController.swift
@@ -0,0 +1,21 @@
+//
+//  TermsOfUseViewController.swift
+//  tomorrow
+//
+//  Created by Russell Ong on 11/11/20.
+//  Copyright © 2020 trillion.unicorn. All rights reserved.
+//
+
+import UIKit
+
+class TermsOfUseViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+
+    @IBAction func backBtnTapped(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+}
